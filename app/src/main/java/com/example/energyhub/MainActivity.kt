11fd3deae26be053dfa38d84aa2b160181fc9612
Ed Rogers <etfrogers@hotmail.com>
2024-04-28 11:26:35 +0100

    Initial commit. Template files and WIP on arrows

---
+++ app/src/main/java/com/example/energyhub/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.energyhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.energyhub.ui.ArrowPreview
+import com.example.energyhub.ui.theme.EnergyHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EnergyHubTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting("Android")
+                    ArrowPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EnergyHubTheme {
+        Greeting("Android")
+    }
+}
